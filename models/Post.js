d13da50da6c8b071ee0109913a7a7a89be171f8b
Welisson luca <welissonluca17@gmail.com>
2021-03-22 00:35:07 -0300

    feat: aggregate decreasing order list tags

--- models/Post.js
+++ models/Post.js
@@ -1,43 +1,44 @@
-const mongoose = require('mongoose');
-const slug = require('slug');
+const mongoose = require("mongoose");
+const slug = require("slug");
 
 mongoose.Promise = global.Promise;
 
 const postSchema = new mongoose.Schema({
-    photo:String,
-    title:{
-        type:String,
-        trim:true,
-        required:'O post precisa de um titulo'
-    },
-    slug:String,
-    body:{
-        type:String,
-        trim:true
-    },
-    tags:[String]
+	photo: String,
+	title: {
+		type: String,
+		trim: true,
+		required: "O post precisa de um titulo",
+	},
+	slug: String,
+	body: {
+		type: String,
+		trim: true,
+	},
+	tags: [String],
 });
 
-postSchema.pre('save',  async function(next)  {
-    if(this.isModified('title')) {
-        this.slug = slug(this.title, { lower: true });
-        
-        const slugRegex = new RegExp(`^(${this.slug})((-[0-9]{1,}$)?)$`, 'i');
+postSchema.pre("save", async function (next) {
+	if (this.isModified("title")) {
+		this.slug = slug(this.title, { lower: true });
 
-        const postWithSlug = await this.constructor.find({slug: slugRegex });
+		const slugRegex = new RegExp(`^(${this.slug})((-[0-9]{1,}$)?)$`, "i");
 
-        if (postWithSlug.length > 0)
-            this.slug = `${this.slug}-${postWithSlug.length + 1}`;
-    }
+		const postWithSlug = await this.constructor.find({ slug: slugRegex });
 
-    next();
+		if (postWithSlug.length > 0)
+			this.slug = `${this.slug}-${postWithSlug.length + 1}`;
+	}
+
+	next();
 });
 
 postSchema.statics.getTagsList = function () {
-    return this.aggregate([
-        { $unwind: '$tags' },
-        {$group: { _id: '$tags', count:{ $sum:1 } } },
-    ]);
-}
+	return this.aggregate([
+		{ $unwind: "$tags" },
+        { $group: { _id: "$tags", count: { $sum: 1 } } },
+        {$sort: { count: -1 } }
+	]);
+};
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model("Post", postSchema);
