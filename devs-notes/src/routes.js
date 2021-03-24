abfe669fd36c230b793bf2b194ebf87c85029861
Welisson luca <welissonluca17@gmail.com>
2021-03-24 01:55:39 -0300

    feat: new routes add, get,post,update,delete

--- devs-notes/src/routes.js
+++ devs-notes/src/routes.js
@@ -4,4 +4,15 @@ const NoteController = require('./controllers/NoteController')
 router.get('/ping', NoteController.ping
 );
 
+routes.get('/note', NoteController.all);
+routes.get("/note/:id", NoteController.one);
+
+routes.post("/note", NoteController.new);
+
+
+routes.put("/note/:id", NoteController.edit);
+
+routes.delete("/note/:id",NoteController.delete);
+
+
 module.exports = router;
