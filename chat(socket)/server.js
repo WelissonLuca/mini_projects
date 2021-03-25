f58f6eff2602ac7397397691e35f247c380d3e03
Welisson luca <welissonluca17@gmail.com>
2021-03-24 22:46:09 -0300

    feat: connection socket io create

--- chat(socket)/server.js
+++ chat(socket)/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
-const htpp = require('http')
+const htpp = require('http');
+const socketIO = require('socket.io');
 
 
 const app = express();
 
-const server = htpp.createServer(app)
-
-server.listen(3000)
+const server = htpp.createServer(app);
+const io = socketIO(server);
+server.listen(3000);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+io.on('connection', (socket) => {
+  console.log('Conexão detectada....');
+  
+  
+  
+});
