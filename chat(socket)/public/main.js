72770613dc1cea530f05a32d50d018f8376de181
Welisson luca <welissonluca17@gmail.com>
2021-03-24 23:31:29 -0300

    feat: verification user chat using

--- chat(socket)/public/main.js
+++ chat(socket)/public/main.js
@@ -1,1 +1,22 @@
 const socket = io();
+let username = '';
+let userList = [];
+
+let loginPage = document.querySelector('#loginPage');
+let chatPage = document.querySelector('#chat');
+
+let loginInput = document.querySelector('#loginNameInput');
+let textInput = document.querySelector('#chatTextInput');
+console.log(loginInput,loginPage,chatPage,textInput)
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
+
+loginInput.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    let name = loginInput.value.trim();
+    if (name != '') {
+      username = name;
+      document.title = 'chat ('+username+')'
+    }
+  }
+})
