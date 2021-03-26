3d13429615b4552b129dd118d9f3d136d054dcec
Welisson luca <welissonluca17@gmail.com>
2021-03-26 12:19:47 -0300

    feat: list infos user created

--- olx_backend/src/controllers/UserController.js
+++ olx_backend/src/controllers/UserController.js
@@ -1,5 +1,7 @@
 const State = require('../models/State');
-
+const User = require('../models/User');
+const Category = require('../models/Category');
+const Ad = require('../models/Ad');
 module.exports = {
 	getStates: async (req, res) => {
 		let states = await State.find();
@@ -8,7 +10,23 @@ module.exports = {
 		res.json({ states });
 	},
 	info: async (req, res) => {
+		let token = req.query.token;
+		const user = await User.findOne({ token });
+		const state = await State.findById(user.state)
+		const ads = await ad.find({ idUser: user_id.toString() });
+
+		let adList = []
+		for (let in ads) {
+			const cat = await Category.findById(ads[i].category)
 		
+			adList.push({...ads[i], category: cat.slug})
+		}
+		res.json({
+			name: user.name,
+			email: user.email,
+			state: state.name,
+			ads: adList
+		});
 	},
 	editAction: async (req, res) => {},
 };
