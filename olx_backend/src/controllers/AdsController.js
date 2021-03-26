5815e082730e64fe3fb1db8e638011b5e29fc291
Welisson luca <welissonluca17@gmail.com>
2021-03-26 18:45:03 -0300

    feat: list categories complete

--- olx_backend/src/controllers/AdsController.js
+++ olx_backend/src/controllers/AdsController.js
@@ -1,5 +1,18 @@
+const Category = require("../models/Category");
 module.exports = {
-	getCategories: async (req, res) => {},
+	getCategories: async (req, res) => {
+		const cat = await Category.find();
+
+		let categories = [];
+
+		for (let i in cats) {
+			categories.push({
+				...cats[i]._doc,
+				img: `${process.env.BASE}/assets/images/${cats[i].slug}.png`,
+			});
+		}
+		res.json({ categories });
+	},
 	addAction: async (req, res) => {},
 	getList: async (req, res) => {},
 	getItem: async (req, res) => {},
