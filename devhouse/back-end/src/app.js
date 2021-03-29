59b001d896f7c2e53af115402433696cbb560aea
Welisson luca <welissonluca17@gmail.com>
2021-03-29 01:26:36 -0300

    feat: implementation mongodb cluster created

--- devhouse/back-end/src/app.js
+++ devhouse/back-end/src/app.js
@@ -1,13 +1,18 @@
 import express from "express";
-
+import mongoose from 'mongoose';
 import routes from "./routes";
 
 class App {
 	constructor() {
 		this.server = express();
 
-	
-
+        mongoose.connect(
+			"mongodb+srv://devhouse:240306@devhouse.q7tic.mongodb.net/devhouse?retryWrites=true&w=majority"
+            , {
+                useNewUrlParser: true,
+                userUnifiedTopology: true
+        });
+		
 		this.middlewares();
 		this.routes();
 	}
